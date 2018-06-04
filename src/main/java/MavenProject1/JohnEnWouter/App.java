81300cd58b08d1bf1ff072e3fda62935a8203c24
Wouter Oude Engberink <wouterclosesure@MacBook-Pro-van-Wouter.local>
2018-06-04 20:26:20 +0200

    eerste commit JohnEnWouter

---
+++ src/main/java/MavenProject1/JohnEnWouter/App.java
@@ -0,0 +1,13 @@
+package MavenProject1.JohnEnWouter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
